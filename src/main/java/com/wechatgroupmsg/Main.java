2d82172f9a90ff91e94f79de43b2b51bcb50265b
xixiaoyong <xixiaoyong@xiaomi.com>
2023-05-05 14:15:56 +0800

    this is auto commit

--- src/main/java/com/wechatgroupmsg/Main.java
+++ src/main/java/com/wechatgroupmsg/Main.java
@@ -1,5 +1,8 @@
 package com.wechatgroupmsg;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
